66f9ef50c42417cf114a44109be92b9f36a5b936
Claudiomiro Lourenco <clausou@hotmail.com>
2020-07-07 19:19:14 -0300

    First JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var nome = "Claudiomiro Lourenço";
+var idade = 37;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade + " anos ");
+//alert (idade + idade2)
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
